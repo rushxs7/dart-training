c64efbf9e281c860bb75eeff5e67fc5a751ac2ba
Rushil Ramautar <rushil.ramautar@gmail.com>
2024-07-16 17:40:55 -0300

    Strings and constants

--- section-01/02-strings.dart
+++ section-01/02-strings.dart
@@ -1,11 +1,14 @@
 void main() {
   var name = 'Rushil';
   print('Hello ' + name + '!');
-
+  
   // You may not change the type of a variable at runtime.
   // name = 15;
+  
   // But you can change the variable to a value of the same type
   name = "Mario";
 
   print('Hello ' + name + '!');
+  // You can also print out variables in strings like this.
+  print('Hello $name!');
 }
---
+++ section-01/03-constants.dart
@@ -0,0 +1,15 @@
+void main() {
+  final name = 'Rushil';
+  const age = 26;
+  print('Hello ' + name + '! You are ' + age.toString() + 'years old.');
+
+  // name = 'Mario';
+  // age = 25;
+  // print('Hello ' + name + '! You are ' + age.toString() + 'years old.');
+
+  print(age + 10);
+  print(age - 10);
+  print(age * 10);
+  print(age / 2);
+  print(age % 2);
+}
